02ffd1a3a6fc467a8b78c1731d4b36609a9c7514
NHorder <nathanhorder@gmail.com>
2026-03-18 18:07:44 +0000

    Testing with LiDAR - can only get LiDAR displayed using Plotly

--- src/sensor_processing_py_pkg/sensor_processing_py_pkg/lidar_processing.py
+++ src/sensor_processing_py_pkg/sensor_processing_py_pkg/lidar_processing.py
@@ -1,14 +1,24 @@
 
-
-
-
-
-
-
-
+#%%
+import open3d as o3d
+import numpy as np
+import plotly.graph_objects as go
 
 
 #%%
-if __name__ == "__main__":
-    pass
+dataset = o3d.data.PLYPointCloud()
+pcd = o3d.io.read_point_cloud(dataset.path)
+
+
+#%%
+points = np.asarray(pcd.points)
+points[:,1] *= -1
+fig = go.Figure(data=[go.Scatter3d(
+    x=points[:,0],
+    y=points[:,2],
+    z=points[:,1],
+    mode='markers',
+    marker=dict(size=2, color=points[:,2], colorscale='Viridis')
+)])
+fig.show()
 # %%
